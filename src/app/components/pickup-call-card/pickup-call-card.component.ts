2818e0d6e6874b968957aeb24607a6b27adc22bf
Siamion Krapivin <homer1996@gmail.com>
2024-12-12 16:51:45 +0100

    feat: update pickup call card component with dynamic header and footer, improved layout, and additional details

--- src/app/components/pickup-call-card/pickup-call-card.component.ts
+++ src/app/components/pickup-call-card/pickup-call-card.component.ts
@@ -5,10 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pickup-call-card.component.html',
   styleUrls: ['./pickup-call-card.component.scss'],
 })
-export class PickupCallCardComponent  implements OnInit {
-
-  constructor() { }
+export class PickupCallCardComponent implements OnInit {
+  hasHeader: boolean = true;
+  hasFooter: boolean = true;
+  constructor() {}
 
   ngOnInit() {}
-
 }
