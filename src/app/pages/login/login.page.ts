b621028c313fa1e9b0cfcdd796ae04995cf4d07d
Siamion Krapivin <homer1996@gmail.com>
2024-12-21 15:36:35 +0100

    feat: add forgot password functionality with loading state management

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AppState } from 'src/app/store/AppState';
+import { Store } from '@ngrx/store';
+import { show, hide } from '../../store/loading/loading.actions';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +13,24 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
   form!: FormGroup;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private store: Store<AppState>
+  ) {}
   ngOnInit() {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  forgotEmailPassword() {
+    this.store.dispatch(show());
+
+    setTimeout(() => {
+      this.store.dispatch(hide());
+    }, 3000);
   }
 
   login() {
