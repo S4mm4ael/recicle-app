602db614c2aa827a0ce645fbd3aad0c584c6c1a8
Siamion Krapivin <homer1996@gmail.com>
2025-01-02 16:48:38 +0100

    feat: enhance registration form validation

--- src/app/pages/register/form/register.page.form.ts
+++ src/app/pages/register/form/register.page.form.ts
@@ -1,5 +1,5 @@
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { repeat } from 'rxjs';
+import { Validators, ValidatorFn } from '@angular/forms';
 
 export class RegisterPageForm {
   private formBuilder: FormBuilder;
@@ -11,25 +11,42 @@ export class RegisterPageForm {
   }
 
   private createForm(): FormGroup {
-    return this.formBuilder.group({
-      name: [''],
-      email: [''],
-      password: [''],
-      repeatPassword: [''],
-      phone: [''],
+    let form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ['', [Validators.required, Validators.minLength(6)]],
+      phone: ['', [Validators.required]],
       address: this.formBuilder.group({
-        street: [''],
-        number: [''],
-        neighborhood: [''],
-        complement: [''],
-        city: [''],
-        state: [''],
-        zip: [''],
+        street: ['', [Validators.required]],
+        number: ['', [Validators.required]],
+        neighborhood: ['', [Validators.required]],
+        complement: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        state: ['', [Validators.required]],
+        zip: ['', [Validators.required]],
       }),
     });
+
+    form.get('repeatPassword')?.setValidators(matchPassword(form));
+
+    return form;
   }
 
   getForm(): FormGroup {
     return this.form;
   }
+}
+
+function matchPassword(form: FormGroup): ValidatorFn {
+  const password = form.get('password');
+  const repeatPassword = form.get('repeatPassword');
+
+  const validator = () => {
+    return password?.value === repeatPassword?.value
+      ? null
+      : { passwordNotMatch: true };
+  };
+
+  return validator;
 }
